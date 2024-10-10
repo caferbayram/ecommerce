2c8636c4ad4996f23ff936f15940658b12e16d98
caferbayram <caferr1834@hotmail.com>
2024-10-11 01:04:05 +0300

    swagger eklendi.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
+FROM maven:3.8.7-openjdk-18-slim AS build
+WORKDIR /app
+COPY pom.xml ./
+COPY src ./src
+RUN mvn clean package -DskipTests
+
 FROM openjdk:18-alpine
 WORKDIR /app
-COPY ./target/ecommerce-0.0.1-SNAPSHOT.jar /app
+COPY --from=build /app/target/ecommerce-0.0.1-SNAPSHOT.jar /app
 ENTRYPOINT ["java", "-jar", "ecommerce-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/enoca/ecommerce/EcommerceApplication.java
+++ src/main/java/com/enoca/ecommerce/EcommerceApplication.java
@@ -3,21 +3,12 @@ package com.enoca.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@RestController
 public class EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
-
-    @RequestMapping()
-    public String home() {
-        return "Welcome to Enoca E-commerce Application";
-    }
-
 }
